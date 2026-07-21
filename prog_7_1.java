6f763ec59d0f90206ccb1dfa0634a82e25e4493e
Jalpin Gadher <jalpin.gadher139412@marwadiuniversity.ac.in>
2026-07-21 15:35:07 +0530

    Add files via upload

---
+++ prog_7_1.java
@@ -0,0 +1,19 @@
+//7.1
+
+import java.util.Scanner;
+
+public class prog_7_1 {
+
+    public static void main(String[] args)
+	{
+		    Scanner scn = new Scanner(System.in);
+			
+			System.out.println("Enter the rupees:");
+			int rupees=scn.nextInt();
+        
+        if (rupees >= 200) 
+		{
+            System.out.println("you have enough money");
+        }
+    }
+}
