ed4fb34e5d1e941503e0bf1954c4e561209e30f6
Jalpin Gadher <jalpin.gadher139412@marwadiuniversity.ac.in>
2026-08-19 15:35:10 +0530

    Add files via upload

---
+++ program18.java
@@ -0,0 +1,10 @@
+public class program18 {
+	public static void main(String[] args)
+	{
+final int nevil_roll_no=101;
+final double max_marks=100.0;
+
+System.out.println("nevil's roll no:"+nevil_roll_no);
+System.out.println("mark's:"+max_marks);
+	}
+}	
